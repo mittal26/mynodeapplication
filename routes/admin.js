cffb95aa8144b990a8c167452e405c2334e96c03
mittal26 <chande.mittal@gmail.com>
2020-03-23 15:48:35 -0400

    title error fix

--- routes/admin.js
+++ routes/admin.js
@@ -24,7 +24,7 @@ async function scrapeData(url, page) {
         const html = await page.evaluate(()=> document.body.innerHTML);
         const $ = await cheerio.load(html);
 
-        let title = $("h1").attr('content');
+        let title = $("h1").getAttribute('content');
         let price = $(".price-characteristic").attr("content");
 
         if(!price) {
